4b325c9933924b822e223370f84f44848e388b0d
baikesun <curious98@naver.com>
2018-11-15 16:04:19 +0900

    fourth

--- main.c
+++ main.c
@@ -1,19 +1,56 @@
 #include <stdio.h>
 
-char *proverb = "All that glisters is not gold.";
-
-void setPointer(char **q)
+int add(int a, int b)
 {
-	*q = proverb;
+	return a+b;
+}
+
+int substract(int a, int b)
+{
+	return a-b;
+}
+
+int multiply(int a, int b)
+{
+	return a*b;
+}
+
+int divide(int a, int b)
+{
+	return a/b;
 }
 
 int main(void)
 {
-	char *p = "zzz";
+	int a, b;
+	char c;
 	
-	setPointer(&p);
+	int (*calculate)(int, int);
 	
-	printf("%s\n", p);
+	printf("수식을 입력하시오 : ");
+	scanf("%d %c %d", &a, &c, &b);
+	
+	switch(c)
+	{
+		case '+': 
+			calculate = add;
+			break;
+		
+		case '-': 
+			calculate = substract;
+			break;
+			
+		case'*': 
+			calculate = multiply;
+			break;
+		
+		case '/'
+		: 
+			calculate = divide;
+			break;		
+	}
+	
+	printf("result : %i\n", calculate(a, b));
 	
 	return 0;
 }
